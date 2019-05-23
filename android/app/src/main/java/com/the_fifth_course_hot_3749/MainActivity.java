4d154dd3f75c6da5dfc706787c650fe343079467
Crowdbotics <team@crowdbotics.com>
2019-05-23 17:51:34 +0000

    initial commit

---
+++ android/app/src/main/java/com/the_fifth_course_hot_3749/MainActivity.java
@@ -0,0 +1,15 @@
+package com.the_fifth_course_hot_3749;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "the_fifth_course_hot_3749";
+    }
+}
